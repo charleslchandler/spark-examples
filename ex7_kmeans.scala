03af9ff96601d6a68590e2592f3977259f4921b6
Chaz Chandler <clc31@inbox.com>
2015-04-17 07:54:32 -0400

    Added zeppelin example

--- ex7_kmeans.scala
+++ ex7_kmeans.scala
@@ -6,6 +6,8 @@ val allK  = 1 to 5
 var WSSSE = Array(0.0,0.0,0.0,0.0,0.0)
 
 val data = sc.textFile("ex7.txt")
+println("%table x\ty\n" + data.map { s => s.split(" ").mkString("\t") }.collect().mkString("\n"))
+
 val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
 
 for(numClusters <- allK) {
@@ -13,7 +15,7 @@ for(numClusters <- allK) {
   WSSSE(numClusters-1) = clusters.computeCost(parsedData)
 }
 
-println("Within Set Sum of Squared Errors = " + WSSSE(numClusters-1))
+println("%table #\tWithin Set Sum of Squared Errors\n" + WSSSE.zipWithIndex.map(p => p._2 + "\t" + p._1).mkString("\n"))
 
 var bestK = 3
 val clusters = KMeans.train(parsedData, bestK, numIterations)
